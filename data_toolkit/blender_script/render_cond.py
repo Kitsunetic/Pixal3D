36c0477c001fbc6ad1c2a67454c4d8205ab44e1a
Kitsunetic <jh.shim.gg@gmail.com>
2026-09-06 09:15:55 +0900

    autoresearch: Decode only the PNG alpha channel during boundary fitting instea

--- data_toolkit/blender_script/render_cond.py
+++ data_toolkit/blender_script/render_cond.py
@@ -378,12 +378,11 @@ def check_mask_boundary_distance(
         - too_far_from_boundary: True if the mask is too far from all boundaries (>80 pixels)
         - min_distance: Minimum distance from mask to any boundary
     """
-    img = Image.open(image_path)
-    if img.mode != 'RGBA':
-        return False, False, 0
-    
-    # Get alpha channel
-    alpha = np.array(img)[:, :, 3]
+    with Image.open(image_path) as img:
+        if img.mode != 'RGBA':
+            return False, False, 0
+
+        alpha = np.array(img.getchannel('A'))
     h, w = alpha.shape
     
     # Find all pixels with alpha > threshold (mask pixels)
